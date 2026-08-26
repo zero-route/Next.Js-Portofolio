545cffdec0850fe1b19adffd0b81d148f3d6cdfa
zero-route <iostream911@gmail.com>
2026-08-26 22:19:25 +0800

    update next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "ghchart.rshah.org" },
+      { protocol: "https", hostname: "i.ytimg.com" },
+    ],
+  },
+};
+
+export default nextConfig;

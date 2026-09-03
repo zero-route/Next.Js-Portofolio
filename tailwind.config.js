ea514e5e867dfc250e9b7d76763ea542eb68271e
zero-route <iostream911@gmail.com>
2026-09-03 21:14:58 +0800

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/app/**/*.{js,jsx}",
     "./src/components/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        // tema Space/Cosmic: hitam - biru - putih
-        space: {
-          black: "#000005",
-          navy: "#0a1230",
-          blue: "#1d4ed8",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
